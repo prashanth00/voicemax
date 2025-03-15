c47fabff9e60069063ed9e3317ac661d18908907
cprashanth00 <cprashanth00@gmail.com>
2025-03-15 18:23:45 +0530

    commit #5

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>umax</h1>
+        <h1>Voice max</h1>
         <p>Unlock your maximum potential</p>
         <a
           href="https://apps.apple.com/app/id123456789"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="app-store-badge.png" alt="Download on the App Store" />
+          <img src="appstore.png" alt="Download on the App Store" />
         </a>
       </header>
       <footer className="footer">
