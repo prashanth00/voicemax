c36c66a8f13dfaa78dc3a9bf50ccca884c1823cb
cprashanth00 <cprashanth00@gmail.com>
2025-04-13 11:26:30 +0530

    css animation added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import "./App.css"; // Import your CSS file
 import { FaFacebookF, FaXTwitter, FaInstagram } from "react-icons/fa6"; // Import icons from react-icons
 import appstore from "./assets/appstore.png";
+import icon from "./assets/icon.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Voice max</h1>
+        <div className="titleAndIcon">
+          <img className="icon" src={icon} alt="Icon" />
+          <h1>My Tone</h1>
+        </div>
         <p>Unlock your maximum potential</p>
         <a
           href="https://apps.apple.com/app/id123456789"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={appstore} alt="Download on the App Store" />
+          <img
+            src={appstore}
+            className="apple-img"
+            alt="Download on the App Store"
+          />
         </a>
       </header>
 
