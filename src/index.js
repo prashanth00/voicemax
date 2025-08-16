24020280a4bb84daedd13d2c1c6d81cbcc2da5d4
cprashanth00 <cprashanth00@gmail.com>
2025-08-16 14:04:06 +0530

    npm install change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import MainRouter from "./MainRouter";
 import { HelmetProvider } from "react-helmet-async";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HelmetProvider>
